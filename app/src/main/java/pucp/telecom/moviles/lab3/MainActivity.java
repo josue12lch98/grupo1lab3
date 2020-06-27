2cfabac92b7dd94acc65aac491e4befd7349df15
Josue <jlopezc@pucp.edu.pe>
2020-06-27 13:00:13 -0500

    merge final

--- app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
+++ app/src/main/java/pucp/telecom/moviles/lab3/MainActivity.java
@@ -12,13 +12,16 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import pucp.telecom.moviles.lab3.Entidades.Grabacion;
 import pucp.telecom.moviles.lab3.Fragments.DialogFragmentEjemplo;
 import pucp.telecom.moviles.lab3.ViewModels.MedicionViewModel;
 import android.view.View;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -31,12 +34,19 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final double referenceAmplitude = 0.0001;
+    MediaRecorder myAudioRecorder=new MediaRecorder();
+    Button buttonIniciarMedicion = (Button) findViewById(R.id.button);
+    Button buttonDetenerMedicion = (Button) findViewById(R.id.button2);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -156,6 +166,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-}
+
+        buttonIniciarMedicion.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            myAudioRecorder.setOutputFile("/dev/null");
+
+            try {
+                myAudioRecorder.prepare();
+                myAudioRecorder.start();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(MainActivity.this, "Medición Iniciada", Toast.LENGTH_LONG).show();
+            buttonIniciarMedicion.setEnabled(false);
+            buttonDetenerMedicion.setEnabled(true);
+            final double maxAmplitude = myAudioRecorder.getMaxAmplitude();
+            final double potencia = 20 * Math.log10(maxAmplitude / referenceAmplitude);
+
+        }
+
+
+    });
+
+        buttonDetenerMedicion.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            myAudioRecorder.stop();
+            buttonDetenerMedicion.setEnabled(false);
+            buttonIniciarMedicion.setEnabled(true);
+            Toast.makeText(MainActivity.this, "Medición Detenida", Toast.LENGTH_LONG).show();
+
+        }
+    });
+
+
+
 
 }
+
